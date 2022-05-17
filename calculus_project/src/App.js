4799a64a0bbef591562a563ff8279baa9d20c9fe
ds12a <david.yz.shen@gmail.com>
2022-05-17 00:02:23 +0000

    Init

---
+++ calculus_project/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid place-content-center text-center h-screen bg-sky-100">
+      <header>
+        <h1 className="">
+          Save Timmy!
+        </h1>
+        <br/><br/><br/>
+        <p>
+          Timmy is a KAMSC student who just finished his calculus final exam. After spending an entire year with the petrifying Mr. Sinclair, he is in desperate need of a vacation. So, he plans a trip to Hawaii. As Timmy is on his flight to Honolulu, tragedy strikes… his plane malfunctions and comes crashing down into the Pacific Ocean! Timmy is at sea for days until he finds an abandoned island — this is where he currently lies. And guess what? Timmy needs your help to get back home! Through solving a series of derivative and integral problems, you have the opportunity to buy Timmy some time. Every point is another day that Timmy survives. The rescue plane flies over the island in 100 days — will Timmy be saved? His fate lies in your hands!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
